09e67bb55272edd4f8208cb3421d6d7e54915333
Rithvik Nishad <rithvikn2001@gmail.com>
2022-05-26 09:56:38 +0530

    create project using cookie-cutter

---
+++ vems/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from vems.users.models import User
+from vems.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
