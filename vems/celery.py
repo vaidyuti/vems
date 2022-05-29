4bdc1ae7812accf4e68180e11c7d47691f043f80
Rithvik Nishad <rithvikn2001@gmail.com>
2022-05-29 10:47:21 +0530

    fix default `DJANGO_SETTINGS_MODULE`

--- vems/celery.py
+++ vems/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vems.settings")
 
 app = Celery("project")
 app.config_from_object("django.conf:settings", namespace="CELERY")
