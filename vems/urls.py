da57a5cc3b8b35ff253d3f50ef311605352e4f0d
Rithvik Nishad <rithvikn2001@gmail.com>
2022-07-18 13:21:34 +0530

    setup redoc and swagger

--- vems/urls.py
+++ vems/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
+from django.urls import include, path, re_path
+from rest_framework import routers, permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from core.views import P2PTransactionViewSet
 from prosumer.views import (
@@ -9,6 +11,20 @@ from prosumer.views import (
     ProsumerViewSet,
     StorageViewSet,
 )
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Vaidyuti API",
+      default_version='v1',
+      description="API for Vaidyuti Energy Management System",
+    #   terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="mail@vaidyuti-placeholder.dummy"),
+      license=openapi.License(name="GNU GPL v3 License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 router = routers.DefaultRouter()
 router.register(r"prosumer/generations", GenerationViewSet)
@@ -24,4 +40,8 @@ urlpatterns = [
     path("api/v1/auth/", include("dj_rest_auth.urls")),
     path("api/v1/", include("openapi.urls")),
     path("api/v1/", include(router.urls)),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
