ade63452ec1fe8731d766f7bf14da0ba2bc1c8ff
Rithvik Nishad <rithvikn2001@gmail.com>
2022-07-18 13:06:51 +0530

    initial migrations

--- vems/settings/dev.py
+++ vems/settings/dev.py
@@ -6,7 +6,7 @@ ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": dj_database_url.config(
-        default="postgresql://vems:vems@localhost:5432/vems",
+        default="postgresql://vems:vems@172.19.0.4:5432/vems",
         conn_max_age=600,
     )
 }
